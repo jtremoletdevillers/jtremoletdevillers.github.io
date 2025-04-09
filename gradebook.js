e2642e13cf4d826a7abb55d3ac01b6d0265320c7
jtremoletdevillers <jtremoletdevillers@my.waketech.edu>
2025-04-09 15:30:03 -0400

    Lesson11 Lab2

---
+++ gradebook.js
@@ -0,0 +1,15 @@
+// Stub function to fetch grade data
+function fetchGradeData() {
+    console.log("fetchGradeData called");
+    // This function will fetch data in a future assignment
+}
+
+// Stub function to populate the gradebook table
+function populateGradebook() {
+    console.log("populateGradebook called");
+    // This function will populate the table in a future assignment
+}
+
+// Call the stub functions to show they are connected
+fetchGradeData();
+populateGradebook();
